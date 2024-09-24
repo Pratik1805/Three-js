ea637461e75e0bc2c664d39a0934fcf07258269e
Pratik1805 <pratikpandey0532@gmail.com>
2024-09-24 17:01:51 +0530

    Added Experience Section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
 
 
     return (
-        <section className={"c-space my-20"}>
+        <section className={"c-space my-20"} id={"contact"}>
             <div className={"relative min-h-screen flex items-center justify-center flex-col"}>
                 <img src={"/assets/terminal.png"} alt={"terminal"} className={"absolute inset-0 min-h-screen sm:block hidden"}/>
                 <div className={"contact-container"}>
